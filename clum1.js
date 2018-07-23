42bd4c7244dd5b38676d24a315d0c16f06ca6af8
lukney <sangeetha18lukney@gmail.com>
2018-07-23 22:51:53 +0530

    edited data

--- clum1.js
+++ clum1.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '',
-	database : 'client',
+	database : 'chat',
 	port :'3306',
 	 debug: false,
         multipleStatements: true
